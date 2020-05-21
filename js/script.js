9346f26106795038459e7cbbff96b31205bb7fcf
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-21 19:00:47 +0200

    Aggiunto click al tasto annulla e relative funzioni

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var formKm = document.getElementById('form-km');
 var formEta = document.getElementById('form-eta');
 var buttonGenera = document.getElementById('genera');
 var buttonAnnulla = document.getElementById('annulla');
+var sezioneBiglietto = document.getElementById('biglietto')
 
 buttonGenera.addEventListener('click',
   function () {
@@ -15,21 +16,40 @@ buttonGenera.addEventListener('click',
       if (etaValue == 'minorenne') {
         prezzoIniziale = kmValue * 0.21;
         prezzoFinale = prezzoIniziale - (prezzoIniziale*0.2)
+        offerta = 'Tariffa -20%'
       } else if (etaValue == 'over') {
         prezzoIniziale = kmValue * 0.21;
         prezzoFinale = prezzoIniziale - (prezzoIniziale*0.4)
+        offerta = 'Tariffa -40%'
       } else {
         prezzoIniziale = kmValue * 0.21
         prezzoFinale = prezzoIniziale
+        offerta = 'Tariffa Standard'
       }
+
       document.getElementById('bigl-nome').innerHTML = nomeValue;
+      document.getElementById('bigl-offerta').innerHTML = offerta;
+      document.getElementById('bigl-carrozza').innerHTML = Math.floor(Math.random() * 10) + 1;
+      document.getElementById('bigl-codice').innerHTML = Math.floor(Math.random() * 10) + 9000;
+      document.getElementById('bigl-costo').innerHTML = prezzoFinale.toFixed(2);
+
+      sezioneBiglietto.className = 'form-wrapper visible'
+
       alert('Biglietto generato correttamente!')
-      console.log(prezzoIniziale)
-      console.log(prezzoFinale)
 
     } else {
 
       alert('Controlla i dati immessi e riprova!')
     }
+  }
+)
+
+buttonAnnulla.addEventListener('click',
+  function () {
+    formNome.value = ''
+    formKm.value = ''
+    formEta.value = 'maggiorenne'
+
+    sezioneBiglietto.className = 'form-wrapper hidden'
   }
 )
